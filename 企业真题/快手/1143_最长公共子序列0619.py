335027160ce19b225c831e3c92d74b18da22da47
Jiehuihuang1107@163.com <Jiehuihuang1107@163.com>
2023-06-20 23:54:19 +0800

    commit

--- 企业真题/快手/1143_最长公共子序列0619.py
+++ 企业真题/快手/1143_最长公共子序列0619.py
@@ -11,8 +11,11 @@
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        len1, len2 = len(text1)+1, len(text2)+1 # 
+
+        len1, len2 = len(text1)+1, len(text2)+1 
+
         dp = [[0 for _ in range(len1)] for _ in range(len2)] # 先对dp数组做初始化操作 shape = [4,6]]
+        
         for i in range(1, len2):
             for j in range(1, len1): # 逐行扫描,大脑里面要有那个矩阵:https://jack-1310222578.cos.ap-guangzhou.myqcloud.com/typora_PicGo/202306192334630.png
                 if text1[j-1] == text2[i-1]: # 第一种情况
