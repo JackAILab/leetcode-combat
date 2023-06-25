fc1adef4d3663cce58bf8c12848cde7c5dfbd207
Jiehuihuang1107@163.com <Jiehuihuang1107@163.com>
2023-06-25 11:50:50 +0800

    merge

--- 企业真题/快手/1143_最长公共子序列0619.py
+++ 企业真题/快手/1143_最长公共子序列0619.py
@@ -15,7 +15,7 @@ class Solution:
         len1, len2 = len(text1)+1, len(text2)+1 
 
         dp = [[0 for _ in range(len1)] for _ in range(len2)] # 先对dp数组做初始化操作 shape = [4,6]]
-        
+
         for i in range(1, len2):
             for j in range(1, len1): # 逐行扫描,大脑里面要有那个矩阵:https://jack-1310222578.cos.ap-guangzhou.myqcloud.com/typora_PicGo/202306192334630.png
                 if text1[j-1] == text2[i-1]: # 第一种情况
@@ -68,10 +68,10 @@ print(solver.longestCommonSubsequence(text1,text2))
 https://www.bilibili.com/video/BV1ey4y1d7oD/?spm_id_from=333.337.search-card.all.click&vd_source=8f32c6c69c90af448f6be495aeb15c05
 
 (1) 定义
-经典LCS问题!
+经典LCS问题! 定义dp[i][j]定义为字符串str1的第i个字符和字符串的第j个字符是否相等
 关键是找出状态方程,然后递归相加即可!一般是需要分解为若干个子问题,如下.
 (2) 分解
-[第一种情况]: 
+[第一种情况]: 重点就是找到一个递推的关系!当某2个字符相等时,他们之前的一段字符(表示为dp[i-1][j-1])也相等,那么这一串就是最长公共子序列了!
 从最后一个情况看,往回推! S1的最后一个字符与S2的最后一个字符相等，那么只需要继续去求前面i-1和j-1字符串中的LCS长度即可 \
 [第二种情况]:
 S1的最后一个字符与S2的最后一个字符不相等，那么就要么把S1的末尾字符舍弃掉，要么把S2的末尾字符舍弃掉 \
